ccd3a442c6bb7fe3290be9f9a2af3c1897cfd696
김승환 <rlatmdghks6287@naver.com>
2024-08-01 17:20:35 +0900

    d

--- chap11/src/sec01/exam08/TimeExam.java
+++ chap11/src/sec01/exam08/TimeExam.java
@@ -1,5 +1,8 @@
 package sec01.exam08;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimeExam {
 
 	public static void main(String[] args) {
@@ -9,10 +12,19 @@ public class TimeExam {
 		long now = System.currentTimeMillis();
 		System.out.println(now);
 		
-		for (int i = 0; i < 1000000000; i++) {
-			
+		for (int i = 0; i < 10000; i++) {
+			System.out.println(i);
 		}
 		System.out.println( System.currentTimeMillis() - now );
+		
+		
+		Date date = new Date();
+		System.out.println(date);
+		
+		String format = "yyyy년 MM월 dd일 HH:mm:ss.SSS";
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		String strDate = sdf.format(date);
+		System.out.println(strDate);
 		
 	}
 
