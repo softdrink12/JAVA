32d32773f7009f9d832db2872b0050b44b71a29d
김승환 <rlatmdghks6287@naver.com>
2024-09-27 16:30:53 +0900

    9/27 spring

---
+++ proj9_REST/src/main/java/human/class1/rest/RestController.java
@@ -0,0 +1,22 @@
+package human.class1.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+//@RestController : @Controller + @ResponseBody
+@RequestMapping("/human")
+public class RestController {
+
+	@RequestMapping("/test1")
+	public int test1() {
+		return 100;
+	}
+	
+	@RequestMapping("/test2")
+	public String test2() {
+		return "ajax";
+	}
+}
